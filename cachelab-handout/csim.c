f37b15ab60cf4fdd8bbaeec1124aeca4af3f0183
Chtholly-Boss <220110915@stu.hit.edu.cn>
2024-02-23 17:45:08 +0800

    Option added and Bug fixed
    Mind that pointer arithmetic will depend on the type.
    There is no need to add sizeof(foo) to pointer arithmetic,
    otherwise it will cause a invalid memory access and result in
    a segfault.

--- cachelab-handout/csim.c
+++ cachelab-handout/csim.c
@@ -1,34 +1,60 @@
 /* Implement the [-hv] option and the -s -E -b -t parameters*/
 /* Error output: Usage() */
 /* L:data load; M:data modify; S:data store; */
+#define _GNU_SOURCE
 #include "cachelab.h"
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <getopt.h>
 // function may be used
 // atoi:convert a string to a interger
-// strcat:cat hit/miss to the original string
 // getopt:get optional speciphy
-// bit level operation
 typedef struct cache_line
 {
     int valid;
-    unsigned long tag;
     int dirty;
+    unsigned long tag;
 }cache_line;
 
-int main(int argc,char** argv){
+int main(int argc,char* argv[]){
     /* Malloc memory for cache */
-    int s = 4;
-    int E = 1;
-    int S = 1 << s;
-    cache_line cache[S][E]; //dynamicly allocate afterwards
-    for (int i = 0; i < S; i++)
+    int sets,lines,blocks;
+    char* filename = NULL;
+    int opt;
+    while ((opt = getopt(argc,argv,"s:E:b:t:")) != -1)
     {
-        for (int j = 0; j < E; j++)
+        switch (opt)
         {
-            cache[i][j].valid = 0;
-            cache[i][j].dirty = 0;
+        case 's':
+            sets = atoi(optarg);
+            break;
+        case 'E':
+            lines = atoi(optarg);
+            break;
+        case 'b':
+            blocks = atoi(optarg);
+            break;
+        case 't':
+            filename = optarg;
+            break;
+        default:
+            break;
+        }
+    }
+    cache_line* cache = (cache_line*)malloc((1 << sets) * lines * sizeof(cache_line));
+    if (!cache)
+    {
+        fprintf(stderr,"can not malloc memory\n");
+        exit(1);
+    }
+    for (int i = 0; i < sets; i++)
+    {
+        for (int j = 0; j < lines; j++)
+        {
+            (cache + i * lines + j)->valid = 0;
+            (cache + i * lines + j)->dirty = 0;
         }
         
     }
@@ -36,7 +62,7 @@ int main(int argc,char** argv){
     int misses = 0;
     int evictions = 0;
     /* process */
-    FILE *trace = fopen(argv[1],"r");
+    FILE *trace = fopen(filename,"r");
     if (!trace)
     {
         fprintf(stderr,"can not open file for reading\n");
@@ -49,28 +75,38 @@ int main(int argc,char** argv){
 
     unsigned long index;
     unsigned long tag;
-
+    unsigned long offset;
+    int block_size;
+    cache_line* cur_cache_line = NULL;
     while (fscanf(trace," %c %lx,%d",&access_type,&address,&size) > 0)
     {   
         // decode address
-        index = address << 56 >> 60;
-        tag = address >> 8;
-        printf("%lx %lx ",index,tag);
+        index = address << (64 - sets - blocks) >> (64 - sets);
+        tag = address >> (sets + blocks);
+        offset = address % (1 << blocks);
+        block_size = 1 << blocks;
+        cur_cache_line = cache + index * lines;
+        printf("%lx %lx %lx ",index,tag,offset);
+        
         // Load
         if (access_type == 'L')
         {
-            if (cache[index][0].valid == 0)
+            if (cur_cache_line->valid == 0)
             {
                 misses += 1;
-                cache[index][0].valid = 1;
-                cache[index][0].tag = tag;
+                cur_cache_line->valid = 1;
+                cur_cache_line->tag = tag;
                 printf("%c %lx,%d %s\n",access_type,address,size,"miss");
-            }else if (cache[index][0].tag != tag)
+            }else if ((cur_cache_line->tag != tag) || (offset > block_size))
             {
                 misses += 1;
                 evictions += 1;
-                cache[index][0].valid = 1;
-                cache[index][0].tag = tag;
+                cur_cache_line->valid = 1;
+                cur_cache_line->tag = tag;
+                if (cur_cache_line->dirty == 1)
+                {
+                    cur_cache_line->dirty = 0;
+                }
                 printf("%c %lx,%d %s\n",access_type,address,size,"miss eviction");
             }else
             {
@@ -81,59 +117,63 @@ int main(int argc,char** argv){
         // Store
         if (access_type == 'S')
         {
-            if (cache[index][0].valid == 0)
+            if (cur_cache_line->valid == 0)
             {
                 misses += 1;
-                cache[index][0].valid = 1;
+                cur_cache_line->valid = 1;
+                cur_cache_line->tag = tag;
                 printf("%c %lx,%d %s\n",access_type,address,size,"miss");
-            }else if (cache[index][0].tag != tag)
+            }else if ((cur_cache_line->tag != tag) || (offset > block_size))
             {
                 misses += 1;
                 evictions += 1;
-                cache[index][0].valid = 1;
-                cache[index][0].tag = tag;
-                if (cache[index][0].dirty == 1)
+                cur_cache_line->valid = 1;
+                cur_cache_line->tag = tag;
+                if (cur_cache_line->dirty == 1)
                 {
-                    cache[index][0].dirty = 0;
+                    cur_cache_line->dirty = 0;
                 }
                 printf("%c %lx,%d %s\n",access_type,address,size,"miss eviction");
             }else
             {
                 hits += 1;
-                cache[index][0].dirty = 1;
+                //here set dirty to 1 will cause segfault
+                cur_cache_line->dirty = 1;
                 printf("%c %lx,%d %s\n",access_type,address,size,"hit");
             }
         }
         //modify
         if (access_type == 'M')
         {
-            if (cache[index][0].valid == 0)
+            if (cur_cache_line->valid == 0)
             {
                 misses += 1;
-                cache[index][0].valid = 1;
-                cache[index][0].tag = tag;
-                printf("%c %lx,%d %s\n",access_type,address,size,"miss hit");
                 hits += 1;
-            }else if (cache[index][0].tag != tag)
+                cur_cache_line->valid = 1;
+                cur_cache_line->tag = tag;
+                printf("%c %lx,%d %s\n",access_type,address,size,"miss hit");
+            }else if ((cur_cache_line->tag != tag) || (offset > block_size))
             {
                 misses += 1;
                 evictions += 1;
-                cache[index][0].valid = 1;
-                cache[index][0].tag = tag;
-                printf("%c %lx,%d %s\n",access_type,address,size,"miss eviction hit");
-                cache[index][0].dirty = 1;
                 hits += 1;
+                cur_cache_line->valid = 1;
+                cur_cache_line->tag = tag;
+                if (cur_cache_line->dirty == 1)
+                {
+                    cur_cache_line->dirty = 0;
+                }
+                printf("%c %lx,%d %s\n",access_type,address,size,"miss eviction hit");
             }else
             {
-                hits += 1;
-                printf("%c %lx,%d %s\n",access_type,address,size,"hit");
-                cache[index][0].dirty = 1;
+                hits += 2;
+                cur_cache_line->dirty = 1;
+                printf("%c %lx,%d %s\n",access_type,address,size,"hit hits");
             }
         }
     }
     printSummary(hits,misses,evictions);
-
+    free(cache);
     fclose(trace);
-
-    /* Final Result */
+    return 0;
 }
